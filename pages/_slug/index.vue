deb18805551a1fec1220880d0f0820f5d6280344
sugoi-reirei <sugoikaizen.kanaya@gmail.com>
2021-10-22 16:15:58 +0900

    recommit

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -6,6 +6,9 @@
     <p class="publishedAt">
       {{ publishedAt }}
     </p>
+    <p class="category">
+      {{ category && category.name }}
+    </p>
     <img style="width: 100%;" :src="mainVisual.url" alt="">
     <div class="post" v-html="content"></div>
   </main>
@@ -28,3 +31,46 @@ export default {
 }
 
 </script>
+
+<style lang="scss" scoped>
+.main {
+  width: 960px;
+  margin: 0 auto;
+}
+
+.title {
+  margin-bottom: 20px;
+}
+
+.publishedAt {
+  margin-bottom: 40px;
+}
+
+.post ::v-deep {
+  & > h1 {
+    font-size: 30px;
+    font-weight: bold;
+    margin: 40px 0 20px;
+    background-color: #eee;
+    padding: 10px 20px;
+    border-radius: 5px;
+  }
+
+  & > h2 {
+    font-size: 24px;
+    font-weight: bold;
+    margin: 40px 0 16px;
+    border-bottom: 1px solid #ddd;
+  }
+
+  & > p {
+    line-height: 1.8;
+    letter-spacing: 0.2px;
+  }
+
+  & > ol {
+    list-style-type: decimal;
+    list-style-position: inside;
+  }
+}
+</style>
